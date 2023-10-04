32a273c2a55bdddfa77c52c609076b6aa9e4c612
alibabaum <143457879+alibabaum@users.noreply.github.com>
2023-10-04 16:14:09 -0600

    Corrections for prefixes plus formatting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
 import java.util.Scanner;
 
 public class Main {
@@ -22,7 +23,8 @@ public class Main {
 
         while (option <= 7) {
             if (option == 1) {
-                System.out.println("Your new string is: " + addContent(firstString));
+                firstString = addContent(firstString);
+                System.out.println("Your new string is: " + firstString);
                 System.out.println("Please choose another option: ");
                 option = inScanner.nextInt();
             } else if (option == 2) {
@@ -46,7 +48,7 @@ public class Main {
                 System.out.println(" ");
                 System.out.println("Please choose another option: ");
                 option = inScanner.nextInt();
-            } else if (option == 7){
+            } else if (option == 7) {
                 System.out.println("Bye.");
                 option = 8;
             } else {
@@ -56,73 +58,80 @@ public class Main {
         }
 
     }
-        public static String addContent (String firstString) {
-            Scanner inScanner = new Scanner(System.in);
-            System.out.println("What would you like to add?");
-            String secondString = inScanner.next();
-            return firstString + " " + secondString;
+
+    public static String addContent(String firstString) {
+        Scanner inScanner = new Scanner(System.in);
+        System.out.println("What would you like to add?");
+        String secondString = inScanner.next();
+        return firstString + secondString;
+    }
+
+    public static void substringChecker(String firstString) {
+        Scanner inScanner = new Scanner(System.in);
+        System.out.println("What substring do you want to check for?");
+        String checkString = inScanner.next();
+        boolean bool = firstString.contains(checkString);
+        if (bool) {
+            System.out.println("Your initial string does contain your indicated substring.");
+        } else {
+            System.out.println("Your initial string does not contain your indicated substring.");
         }
 
-        public static void substringChecker (String firstString){
-            Scanner inScanner = new Scanner(System.in);
-            System.out.println("What substring do you want to check for?");
-            String checkString = inScanner.next();
-            boolean bool = firstString.contains(checkString);
-            if (bool) {
-                System.out.println("Your initial string does contain your indicated substring.");
-            } else {
-                System.out.println("Your initial string does not contain your indicated substring.");
-            }
+    }
 
-        }
-        public static int lengthChecker (String firstString){
-          int length = firstString.length();
-          char last = firstString.charAt(length - 1);
-          return length;
-        }
+    public static int lengthChecker(String firstString) {
+        int length = firstString.length();
+        char last = firstString.charAt(length - 1);
+        return length;
+    }
+
     // The below method is one I made because I misread the assignment. I'm keeping it here for future reference since
     // I struggle with for loops and i feel like it will be useful at some point.
-        public static void afterPrefix (String firstString){
-            int length = firstString.length();
-            //i is function as the limit in this case. print out before it hits three.
-            for (int i = 3; i < length; i++){
-                char prefix = firstString.charAt(i);
-                System.out.print(prefix);
-            }
+    public static void afterPrefix(String firstString) {
+        int length = firstString.length();
+        //i is function as the limit in this case. print out before it hits three.
+        for (int i = 3; i < length; i++) {
+            char prefix = firstString.charAt(i);
+            System.out.print(prefix);
         }
-        public static void afterPrefix2 (String firstString){
-            Scanner inScanner = new Scanner(System.in);
-            System.out.println("Prefix?");
-            String prefix = inScanner.next();
-            int index = firstString.indexOf(prefix);
+    }
 
-            for (int i = index + 1; i < firstString.length(); i++){
-                char prefixChar = firstString.charAt(i);
-                System.out.print(prefixChar);
-            }
-            System.out.println(" ");
+    public static void afterPrefix2(String firstString) {
+        Scanner inScanner = new Scanner(System.in);
+        System.out.println("Prefix?");
+        String prefix = inScanner.next();
+        int index = firstString.indexOf(prefix);
+        int startingPoint = index + prefix.length();
+
+        for (int i = startingPoint; i < firstString.length(); i++) {
+            char prefixChar = firstString.charAt(i);
+            System.out.print(prefixChar);
         }
-    public static void beforeSuffix (String firstString){
+        System.out.println(" ");
+    }
+
+    public static void beforeSuffix(String firstString) {
         Scanner inScanner = new Scanner(System.in);
         System.out.println("Suffix?");
         String suffix = inScanner.next();
         int index = firstString.indexOf(suffix);
-        for (int i = 0; i < index; i++){
+        for (int i = 0; i < index; i++) {
             char suffixChar = firstString.charAt(i);
             System.out.print(suffixChar);
         }
         System.out.println(" ");
     }
 
-    public static void betweenPrefixSuffix (String firstString){
+    public static void betweenPrefixSuffix(String firstString) {
         Scanner inScanner = new Scanner(System.in);
         System.out.println("Prefix?");
         String prefix = inScanner.next();
         System.out.println("Suffix?");
         String suffix = inScanner.next();
         int index1 = firstString.indexOf(prefix);
+        int startingPoint = index1 + prefix.length();
         int index2 = firstString.indexOf(suffix);
-        for (int i = index1 + 1; i <= index2 - 1; i++){
+        for (int i = startingPoint; i <= index2 - 1; i++) {
             char suffixChar = firstString.charAt(i);
             System.out.print(suffixChar);
         }
